50ebdbdebf712588b5c708c3e5425fc96df6eb1d
mmdlow <mmdlow@gmail.com>
2025-02-22 23:40:18 +0800

    Implement form schema and validation

--- src/views/landing/invite/RequestInviteDialog.tsx
+++ src/views/landing/invite/RequestInviteDialog.tsx
@@ -1,13 +1,67 @@
-import { Label } from '@/views/components/label';
-import { Box, Button, Dialog, Flex, TextField } from '@radix-ui/themes';
-import { ComponentPropsWithoutRef, ComponentRef, forwardRef } from 'react';
+import { useRequestInviteMutation } from '@/api/request-invite-mutation';
+import { stripErrorPrefix } from '@/lib/strip-error-prefix';
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/views/components/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Dialog, Flex, TextField } from '@radix-ui/themes';
+import { ComponentPropsWithoutRef, ComponentRef, forwardRef, useEffect, useState } from 'react';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+// TODO message constants
+const FormSchema = z
+  .object({
+    name: z.string({ required_error: 'Name is required' }).min(3, { message: 'Name is too short' }),
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email({ message: 'Invalid email address' }),
+    confirmEmail: z.string().email({ message: 'Invalid email address' }),
+  })
+  .refine(data => data.email === data.confirmEmail, {
+    message: 'Emails do not match',
+    path: ['confirmEmail'],
+  });
+
+const formId = 'request-invite-form';
 
 const RequestInviteDialog = forwardRef<
   ComponentRef<typeof Button>,
   ComponentPropsWithoutRef<typeof Button>
 >((props, ref) => {
+  const [open, setOpen] = useState(false);
+
+  const { mutate: requestInvite, isPending } = useRequestInviteMutation({
+    onSuccess: () => setOpen(false),
+    onError: error => {
+      form.setError('email', {
+        type: 'in_use',
+        message: stripErrorPrefix(error),
+      });
+    },
+  });
+
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+    defaultValues: { name: '', email: '', confirmEmail: '' },
+    mode: 'onTouched',
+  });
+
+  const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async ({ name, email }) => {
+    requestInvite({ name, email });
+  };
+
+  useEffect(() => {
+    if (open) form.reset();
+  }, [open, form]);
+
   return (
-    <Dialog.Root>
+    <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger>
         <Button ref={ref} size={'3'} {...props}>
           Request an invite
@@ -17,28 +71,61 @@ const RequestInviteDialog = forwardRef<
       <Dialog.Content maxWidth={'450px'} aria-describedby={undefined}>
         <Dialog.Title>Request an invite</Dialog.Title>
 
-        <Flex direction={'column'} gap={'3'} py={'3'}>
-          <Box>
-            <Label htmlFor="fullName">Full name</Label>
-            <TextField.Root id="fullName" type="text" />
-          </Box>
+        <FormProvider {...form}>
+          <form id={formId} onSubmit={form.handleSubmit(onSubmit)}>
+            <Flex direction={'column'} gap={'4'} py={'3'}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel required>Full name</FormLabel>
+                    <FormControl>
+                      <TextField.Root {...field} />
+                    </FormControl>
+                    <FormMessage />
+                    {/* TODO extract message */}
+                    <FormDescription>Name should be at least 3 characters.</FormDescription>
+                  </FormItem>
+                )}
+              />
 
-          <Box>
-            <Label htmlFor="email">Email</Label>
-            <TextField.Root id="email" type="email" />
-          </Box>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel required>Email</FormLabel>
+                    <FormControl>
+                      <TextField.Root {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-          <Box>
-            <Label htmlFor="confirmEmail">Confirm email</Label>
-            <TextField.Root id="confirmEmail" type="email" />
-          </Box>
-        </Flex>
+              <FormField
+                control={form.control}
+                name="confirmEmail"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel required>Confirm email</FormLabel>
+                    <FormControl>
+                      <TextField.Root {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </Flex>
 
-        <Flex gap={'2'} justify={'end'} pt={'3'}>
-          <Dialog.Close>
-            <Button>Submit</Button>
-          </Dialog.Close>
-        </Flex>
+            <Flex gap={'2'} justify={'end'} pt={'3'}>
+              <Button type="submit" loading={isPending}>
+                Submit
+              </Button>
+            </Flex>
+          </form>
+        </FormProvider>
       </Dialog.Content>
     </Dialog.Root>
   );
